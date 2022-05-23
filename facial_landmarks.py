0332b0103ebe2b07ae5fcb14578f1ad037f92a55
shubham koli <96383738+MrBam44@users.noreply.github.com>
2022-05-23 22:24:36 +0530

    Add files via upload

---
+++ facial_landmarks.py
@@ -0,0 +1,31 @@
+import cv2
+import mediapipe as mp
+
+# Face Mesh
+mp_face_mesh = mp.solutions.face_mesh
+face_mesh = mp_face_mesh.FaceMesh()
+
+# Image
+a = "E:\Zoom\Face Train/ARATI/art11.png"
+image = cv2.imread("E:\Zoom/Face Train/ARATI/art 5.png")
+image = cv2.imread(a)
+height, width, _ = image.shape
+print("Height, width", height, width)
+rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+# Facial landmarks
+result = face_mesh.process(rgb_image)
+
+for facial_landmarks in result.multi_face_landmarks:
+    for i in range(0, 468):
+        pt1 = facial_landmarks.landmark[i]
+        x = int(pt1.x * width)
+        y = int(pt1.y * height)
+
+        cv2.circle(image, (x, y), 1, (100, 100, 0), -1)
+        #cv2.putText(image, str(i), (x, y), 0, 1, (0, 0, 0))
+
+
+
+cv2.imshow("Image", image)
+cv2.waitKey(0)
